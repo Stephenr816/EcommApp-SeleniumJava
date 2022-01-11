2d65d6fdce3cabed9b8d67a323001655453f53e8
Stephenr816 <stephenr816@gmail.com>
2022-01-11 19:14:02 +0530

    Element Util added

--- src/main/java/seleniumSeries/AmazonAppTest.java
+++ src/main/java/seleniumSeries/AmazonAppTest.java
@@ -1,13 +1,25 @@
 package seleniumSeries;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class AmazonAppTest {
+	
+	static WebDriver driver;
 
 	public static void main(String[] args) {
 		
 		BrowserUtil br = new BrowserUtil();
-		br.init_driver("chrome");
+		driver = br.init_driver("chrome");
 		br.launchUrl("https://www.amazon.in/");
 		String title = br.getPageTitle();
+		
+		ElementUtil el = new ElementUtil(driver);
+		
+		By emailId = By.id("input-email");
+		By password = By.id("input-password");
+		
+		el.doSendKeys(emailId, "naveen@gmail.com");
 
 	}
 
--- src/main/java/seleniumSeries/LocatorsConcept.java
+++ src/main/java/seleniumSeries/LocatorsConcept.java
@@ -2,19 +2,38 @@ package seleniumSeries;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LocatorsConcept {
+	
+	static WebDriver driver;
 
 	public static void main(String[] args) {
 		
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		
-		driver.findElement(By.id("input-email")).sendKeys("stephen@gmail.com");
+		By emailId = By.id("input-email");
+		By password = By.id("input-password");
+		
+		doSendKeys(emailId, "naveen@gmail.com");
+		doSendKeys(password, "naveen123");
+		
+		
 
+	}
+	
+	public static WebElement getElement(By locator) {
+		
+		return driver.findElement(locator);
+	}
+	
+	public static void doSendKeys(By locator, String value) {
+		
+		getElement(locator).sendKeys(value);
 	}
 
 }
